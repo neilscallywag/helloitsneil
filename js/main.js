44ce24a22d41ea195293a029e57e42d61f497396
Neil <64523806+neilscallywag@users.noreply.github.com>
2021-10-06 14:39:37 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,82 +1,146 @@
-	var Page = (function() {
+ (function($) { 
+
 	
-				var config = {
-						$bookBlock : $( '#bb-bookblock' ),
-						$navNext : $( '#bb-nav-next' ),
-						$navPrev : $( '#bb-nav-prev' ),
-						$navFirst : $( '#bb-nav-first' ),
-						$navLast : $( '#bb-nav-last' )
-					},
-					init = function() {
-						config.$bookBlock.bookblock( {
-							speed : 1000,
-							shadowSides : 0.8,
-							shadowFlip : 0.4
-						} );
-						initEvents();
-					},
-					initEvents = function() {
-						
-						var $slides = config.$bookBlock.children();
+	//Navigation
 
-						// add navigation events
-						config.$navNext.on( 'click touchstart', function() {
-							config.$bookBlock.bookblock( 'next' );
-							return false;
-						} );
+	var app = function () {
+		var body = undefined;
+		var menu = undefined;
+		var menuItems = undefined;
+		var init = function init() {
+			body = document.querySelector('body');
+			menu = document.querySelector('.menu-icon');
+			nav = document.querySelector('.nav__content');
+			menuItems = document.querySelectorAll('.nav__list-item');
+			applyListeners();
+		};
+		var applyListeners = function applyListeners() {
+			menu.addEventListener('click', function () {
+			    toggleClass(nav, 'animated')
+				return toggleClass(body, 'nav-active');
+			});
+		};
+		var toggleClass = function toggleClass(element, stringClass) {
+			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+		};
+		init();
+	}();
 
-						config.$navPrev.on( 'click touchstart', function() {
-							config.$bookBlock.bookblock( 'prev' );
-							return false;
-						} );
+	
+})(jQuery); 
 
-						config.$navFirst.on( 'click touchstart', function() {
-							config.$bookBlock.bookblock( 'first' );
-							return false;
-						} );
 
-						config.$navLast.on( 'click touchstart', function() {
-							config.$bookBlock.bookblock( 'last' );
-							return false;
-						} );
-						
-						// add swipe events
-						$slides.on( {
-							'swipeleft' : function( event ) {
-								config.$bookBlock.bookblock( 'next' );
-								return false;
-							},
-							'swiperight' : function( event ) {
-								config.$bookBlock.bookblock( 'prev' );
-								return false;
-							}
-						} );
 
-						// add keyboard events
-						$( document ).keydown( function(e) {
-							var keyCode = e.keyCode || e.which,
-								arrow = {
-									left : 37,
-									up : 38,
-									right : 39,
-									down : 40
-								};
+let post_header = document.querySelectorAll('#post');
 
-							switch (keyCode) {
-								case arrow.left:
-									config.$bookBlock.bookblock( 'prev' );
-									break;
-								case arrow.right:
-									config.$bookBlock.bookblock( 'next' );
-									break;
-							}
-						} );
-					};
 
-					return { init : init };
 
-			})();
-			
-			Page.init();
-			
- 
+const controller = new ScrollMagic.Controller();
+
+post_header.forEach(head => {
+  let timeline = new TimelineMax();
+  let heading = head.querySelector('h3');
+  let postContent = head.querySelector('p');
+  let link = head.querySelector('.link');
+  let counter = head.querySelector('.counter');
+  let image = head.querySelector('.image');
+
+  timeline.
+  from(counter, .5, {
+    x: 30,
+    autoAlpha: 0,
+    ease: Expo.ease }).
+
+
+  from(image, .5, {
+    x: -30,
+    autoAlpha: 0,
+    ease: Expo.ease }).
+
+
+  from(heading, .3, {
+    y: 30,
+    autoAlpha: 0,
+    ease: Expo.ease }).
+
+
+  from(postContent, .3, {
+    y: 30,
+    autoAlpha: 0,
+    ease: Expo.ease }).
+
+
+  from(link, .3, {
+    x: 30,
+    autoAlpha: 0,
+    ease: Expo.ease });
+
+
+  const scene = new ScrollMagic.Scene({
+    triggerElement: head,
+    triggerHook: .5,
+    reverse: true }).
+
+
+  setTween(timeline).
+  addTo(controller);
+
+
+});
+
+"use strict";
+let cursor = $(".cursor")
+  , follower = $(".follower")
+  , cWidth = 8
+  , fWidth = 48
+  , delay = 10
+  , mouseX = 0
+  , mouseY = 0
+  , posX = 0
+  , posY = 0;
+TweenMax.to({}, .001, {
+    repeat: -1,
+    onRepeat: function() {
+        posX += (mouseX - posX) / delay,
+        posY += (mouseY - posY) / delay,
+        TweenMax.set(follower, {
+            css: {
+                left: posX - fWidth / 2,
+                top: posY - fWidth / 2
+            }
+        }),
+        TweenMax.set(cursor, {
+            css: {
+                left: mouseX - cWidth / 2,
+                top: mouseY - cWidth / 2
+            }
+        })
+    }
+}),
+$(document).on("mousemove", function(e) {
+    mouseX = e.clientX,
+    mouseY = e.clientY
+}),
+$("a").on({
+    mouseenter: function() {
+        cursor.addClass("is-active"),
+        follower.addClass("is-active")
+    },
+    mouseleave: function() {
+        cursor.removeClass("is-active"),
+        follower.removeClass("is-active")
+    }
+}),
+window.onload = function() {
+    function e() {
+        $(".fadeIn").each(function() {
+            let e = $(this).offset().top;
+            $(window).scrollTop() > e - $(window).height() && $(this).addClass("view")
+        })
+    }
+    e(),
+    $(window).scroll(function() {
+        e()
+    })
+}
+;
